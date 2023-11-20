8b4be15905d7bb156b698169c8bd8eab7ca8c35b
Tomás Ricardo <tomasricardo@MBP-de-Tomas.home>
2023-11-20 21:25:07 +0000

    Primeira Versão - Home, Contactos e Sobre

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* Validação de campos Input vazios (através de Bootstrap) */
+'use strict'
+
+const forms = document.querySelectorAll('.needs-validation')
+
+// Loop over them and prevent submission
+Array.from(forms).forEach(form => {
+  form.addEventListener('submit', event => {
+    if (!form.checkValidity()) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    form.classList.add('was-validated')
+  }, false)
+})
+
+
+
